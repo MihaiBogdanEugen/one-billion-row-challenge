726a9f202d2f999579e0eeac719d5a694279713f
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2024-01-06 15:38:51 +0100

    no need to pass along the name => 10% faster

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use clap::Parser;
 use rayon::iter::ParallelIterator;
 use rayon::prelude::*;
 use rustc_hash::FxHashMap;
-use std::fmt::Display;
-use std::fmt::Formatter;
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
@@ -11,71 +10,53 @@ use std::path::PathBuf;
 use std::time::Duration;
 use std::time::Instant;
 
-struct Measurement {
-    name: String,
-    temperature: i64,
-}
-
-impl From<&str> for Measurement {
-    fn from(line: &str) -> Self {
-        let (name_as_str, temperature_as_str) = line.split_once(';').unwrap();
-        let name: String = name_as_str.to_owned();
-        let temperature: i64 = (temperature_as_str.parse::<f64>().unwrap() * 10.0) as i64;
-        Measurement { name, temperature }
-    }
-}
-
 struct Statistics {
-    name: String,
-    min: i64,
-    max: i64,
-    sum: i64,
+    curr_min: i64,
+    curr_max: i64,
+    acc_sum: i64,
     count: usize,
+    min: f64,
+    max: f64,
+    mean: f64,
+}
+
+fn round_one_digit_precision(x: f64) -> f64 {
+    format!("{:.1$}", x, 1).parse::<f64>().unwrap()
 }
 
 impl Statistics {
-
-    fn get_min(&self) -> f64 {
-        format!("{:.1$}", (self.min as f64 / 10.0), 1)
-            .parse::<f64>()
-            .unwrap()        
+    fn update(&mut self, temperature: i64) {
+        self.curr_min = self.curr_min.min(temperature);
+        self.curr_max = self.curr_max.max(temperature);
+        self.acc_sum += temperature;
+        self.count += 1;
     }
 
-    fn get_max(&self) -> f64 {
-        format!("{:.1$}", (self.max as f64 / 10.0), 1)
-            .parse::<f64>()
-            .unwrap()        
+    fn merge(&mut self, other: Statistics) {
+        self.curr_min = self.curr_min.min(other.curr_min);
+        self.curr_max = self.curr_max.max(other.curr_max);
+        self.acc_sum += other.acc_sum;
+        self.count += other.count;
     }
 
-    fn get_mean(&self) -> f64 {
-        format!("{:.1$}", (self.sum as f64 / (10.0 * self.count as f64)), 1)
-            .parse::<f64>()
-            .unwrap()
+    fn compute(&mut self) {
+        self.min = round_one_digit_precision(self.curr_min as f64 / 10.0);
+        self.max = round_one_digit_precision(self.curr_max as f64 / 10.0);
+        self.mean = round_one_digit_precision(self.acc_sum as f64 / (10.0 * self.count as f64));
     }
 }
 
 impl Default for Statistics {
     fn default() -> Self {
         Self {
-            name: String::from(""),
-            min: i64::MAX,
-            max: i64::MIN,
-            sum: 0,
+            curr_min: i64::MAX,
+            curr_max: i64::MIN,
+            acc_sum: 0,
             count: 0,
+            max: 0.0,
+            min: 0.0,
+            mean: 0.0,
         }
-    }
-}
-
-impl Display for Statistics {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}={}/{}/{}",
-            self.name,
-            self.get_min(),
-            self.get_max(),
-            self.get_mean()
-        )
     }
 }
 
@@ -91,51 +72,59 @@ fn main() {
     let path: PathBuf = PathBuf::from(&cli.path);
     let input: String = std::fs::read_to_string(path).unwrap();
 
-    let fold_op = |mut acc: FxHashMap<String, Statistics>, m: Measurement| -> FxHashMap<String, Statistics> {
-        let stats: &mut Statistics = acc.entry(m.name.to_string()).or_default();
-        stats.name = m.name;
-        stats.min = stats.min.min(m.temperature);
-        stats.max = stats.max.max(m.temperature);
-        stats.sum += m.temperature;
-        stats.count += 1;
+    let filter_op = |line: &&str| -> bool { !line.is_empty() };
+
+    let map_op = |line: &str| -> (String, i64) {
+        let (name_as_str, temperature_as_str) = line.split_once(';').unwrap();
+        let name: String = name_as_str.to_owned();
+        let temperature: i64 = (temperature_as_str.parse::<f64>().unwrap() * 10.0) as i64;
+        (name, temperature)
+    };
+
+    let fold_op = |mut acc: FxHashMap<String, Statistics>,
+                   (name, temperature): (String, i64)|
+     -> FxHashMap<String, Statistics> {
+        let stats: &mut Statistics = acc.entry(name.to_string()).or_default();
+        stats.update(temperature);
         acc
     };
 
-    let op = |mut acc: FxHashMap<String, Statistics>, map: FxHashMap<String, Statistics>| -> FxHashMap<String, Statistics> {
+    let reduce_op = |mut acc: FxHashMap<String, Statistics>,
+                     map: FxHashMap<String, Statistics>|
+     -> FxHashMap<String, Statistics> {
         for (name, stats) in map {
             let acc_stats: &mut Statistics = acc.entry(name).or_default();
-            acc_stats.name = stats.name;
-            acc_stats.min = acc_stats.min.min(stats.min);
-            acc_stats.max = acc_stats.max.max(stats.max);
-            acc_stats.sum += stats.sum;
-            acc_stats.count += stats.count;
+            acc_stats.merge(stats);
         }
         acc
     };
 
+    let identity = FxHashMap::<String, Statistics>::default;
+
     let now: Instant = Instant::now();
-    let result: FxHashMap<String, Statistics> = input
+
+    let mut result: FxHashMap<String, Statistics> = input
         .par_lines()
-        .filter(|line: &&str| !line.is_empty())
-        .map(|line: &str| Measurement::from(line))
-        .fold(
-            FxHashMap::<String, Statistics>::default,
-            fold_op,
-        )
-        .reduce(FxHashMap::<String, Statistics>::default, op);
+        .filter(filter_op)
+        .map(map_op)
+        .fold(identity, fold_op)
+        .reduce(identity, reduce_op);
+    result
+        .values_mut()
+        .for_each(|stats: &mut Statistics| stats.compute());
+
     let duration: Duration = now.elapsed();
     println!("Results for {} generated in {:?}", &cli.path, duration);
 
     let file: File = File::create(cli.path.replace("measurements", "results")).unwrap();
     let mut writer: BufWriter<File> = BufWriter::new(file);
 
-    let mut sorted_result: Vec<&Statistics> =
-        result.values().collect::<Vec<&Statistics>>();
-    sorted_result.sort_by(
-        |a: &&Statistics, b: &&Statistics| a.name.cmp(&b.name),
-    );
-
-    for statistics in sorted_result {
-        writeln!(writer, "{}", statistics).unwrap();
+    for (name, stats) in BTreeMap::from_par_iter(result.into_par_iter()) {
+        writeln!(
+            writer,
+            "{}={}/{}/{}",
+            name, stats.min, stats.max, stats.mean
+        )
+        .unwrap();
     }
 }
