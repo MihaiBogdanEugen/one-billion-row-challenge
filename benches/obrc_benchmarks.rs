4f257dd84a7fb519db12df633e6bdfd86e34fdef
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2024-11-29 13:12:05 +0100

    benchmarking w/ criterion

---
+++ benches/obrc_benchmarks.rs
@@ -0,0 +1,35 @@
+use std::hint::black_box;
+
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::Bencher;
+use criterion::Criterion;
+use obrc::solutions::solution_naive::SolutioNaive;
+use obrc::solutions::solution_rayon_fxhash::SolutionRayonFxHash;
+use obrc::solutions::solver::Solver;
+
+fn benchmark_solution_naive(c: &mut Criterion) {
+    c.bench_function("SolutioNaive", |b: &mut Bencher<'_>| {
+        b.iter(|| SolutioNaive::solve_obrc(black_box("resources/measurements_1000000.txt")))
+    });
+}
+
+fn benchmark_solution_rayon_fxhash(c: &mut Criterion) {
+    c.bench_function("SolutionRayonFxHash", |b: &mut Bencher<'_>| {
+        b.iter(|| SolutionRayonFxHash::solve_obrc(black_box("resources/measurements_1000000.txt")))
+    });
+}
+
+criterion_group! {
+    name = benches_naive;
+    config = Criterion::default().sample_size(20);
+    targets = benchmark_solution_naive
+}
+
+criterion_group! {
+    name = benches_rayon_fxhash;
+    config = Criterion::default().sample_size(20);
+    targets = benchmark_solution_rayon_fxhash
+}
+
+criterion_main!(benches_naive, benches_rayon_fxhash);
